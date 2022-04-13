29ff07eac424d2ba3fd21ba2231b82483b3adfb9
nguyen <pnu2410@gmail.com>
2022-04-13 15:39:33 +0700

    cái này mới là tạo project và app

---
+++ KingOfCake/urls.py
@@ -0,0 +1,9 @@
+from sys import path_hooks
+from xml.etree.ElementInclude import include
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('cake/', include("manageCake.urls")),
+]
