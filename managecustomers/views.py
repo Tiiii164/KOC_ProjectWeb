8d2f584b44c6f89722b57561fd7225002cab698c
Ti <tipham077@gmail.com>
2022-05-03 13:30:25 +0700

    1

--- managecustomers/views.py
+++ managecustomers/views.py
@@ -7,3 +7,27 @@ from django import views
 def show_customer(request):
     context = {}
     return render(request, "customer/customer.html", context)
+
+
+def details(request):
+    context = {}
+    return render(request, "customer/customer.html", context)
+
+
+def add(request):
+    context = {}
+    return render(request, "customer/customer.html", context)
+
+
+def delete(request):
+    context = {}
+    return redirect(show_customer)
+
+
+def update(request):
+    context = {}
+    return render(request, "customer/customer.html", context)
+
+
+def back_to_list(request):
+    return redirect(show_customer)
