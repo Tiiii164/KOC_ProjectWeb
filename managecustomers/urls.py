e29cff9fc2e59c4884e67100636dc4d2a8dc6c53
Ti <tipham077@gmail.com>
2022-05-04 14:11:25 +0700

    thêm nhiều lắm đó xin đừng mất gì hết

--- managecustomers/urls.py
+++ managecustomers/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.show_customer),
-    path("/<int:id>", views.details),
-    path('/add', views.add),
-    path('/delete/<int:id>', views.delete),
-    path('/update/<int:id>', views.update),
-    path('/back_to_list', views.back_to_list),
+    path("<int:id>", views.details),
+    path('add', views.add),
+    path('delete/<int:id>', views.delete),
+    path('edit/<int:id>', views.edit),
+    path('back_to_list', views.back_to_list),
 ]
--- managecustomers/views.py
+++ managecustomers/views.py
@@ -1,32 +1,53 @@
-from django.shortcuts import render, redirect
-from django import views
+from django.shortcuts import render, redirect, get_object_or_404
 
+from django.contrib.auth.models import User
+from managecustomers.forms import UserForm
 # Create your views here.
 
 
 def show_customer(request):
-    context = {}
+    users = User.objects.all()
+    context = {
+        "users": users,
+    }
     return render(request, "customer/customer.html", context)
 
 
-def details(request):
-    context = {}
-    return render(request, "customer/customer.html", context)
+def details(request, id):
+    # user = User.objects.get(id=id) này là lấy user theo id
+    user = get_object_or_404(User, id=id)
+    context = {
+        "user": user,
+    }
+    return render(request, "customer/details.html", context)
 
 
 def add(request):
+    forms = UserForm(request.POST or None)
+
+    if forms.is_valid():
+        username = request.POST["username"]
+        password = request.POST["password"]
+        email = request.POST["email"]
+
+        user = User.objects.create_user(username, email.password)
+
+        user.save()
+
+        return redirect(show_customer)
     context = {}
-    return render(request, "customer/customer.html", context)
+    return render(request, "customer/add.html", context)
 
 
-def delete(request):
-    context = {}
+def delete(request, id):
+    user = get_object_or_404(User, id=id)
+    user.delete()
     return redirect(show_customer)
 
 
-def update(request):
+def edit(request, id):
     context = {}
-    return render(request, "customer/customer.html", context)
+    return render(request, "customer/update.html", context)
 
 
 def back_to_list(request):
