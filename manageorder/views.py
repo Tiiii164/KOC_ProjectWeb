5ccdc9446c7b7742fc5c36c305ef642a565a655a
vubaoduygcs210006 <vubaoduy2205@gmail.com>
2022-05-05 22:42:16 +0700

    thêm fix dtb order page

--- manageorder/views.py
+++ manageorder/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from manageCake.models import Product
 # Create your views here.
-def show_order(request):
-    return render(request, "order/order.html") 
+def product_order(request):
+    products = Product.objects.all()
+    context = {
+        "products": products
+    }
+    return render(request, "order/order.html", context)
