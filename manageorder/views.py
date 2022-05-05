7616e437831a8bbb48bd2dbad55509c3ec14a3cc
vubaoduygcs210006 <vubaoduy2205@gmail.com>
2022-05-05 22:36:56 +0700

    thêm dtb order page

--- manageorder/views.py
+++ manageorder/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_order(request):
-    return render(request, "order/order.html")
+    return render(request, "order/order.html") 
